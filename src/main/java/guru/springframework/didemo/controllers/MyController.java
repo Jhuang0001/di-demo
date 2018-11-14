5ee12e65422743d44aeca2aa87db1440c65f1730
jimmyhuang <jimmyhuang0001@gmail.com>
2018-11-14 13:11:00 -0500

    Initial commit

---
+++ src/main/java/guru/springframework/didemo/controllers/MyController.java
@@ -0,0 +1,10 @@
+package guru.springframework.didemo.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+    public void print(){
+        System.out.println("Hello world");
+    }
+}
